e0355d7c493a99dea19ec6d55a5c7f6533703908
MariaLight <mdlight@mail.ru>
2021-04-21 15:20:49 +0300

    modal&swipers added

--- js/main.js
+++ js/main.js
@@ -1,27 +1,78 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-    let mySwiper = new Swiper('.swiper-container', {
+  let animSwiper = new Swiper('.anim-container', {
+    slideClass: 'anim-slide',
+    wrapperClass: 'anim-wrapper',
+    direction: 'horizontal',
+    loop: true,
+    navigation: {
+      nextEl: '.anim-button-next',
+      prevEl: '.anim-button-prev',
+    },
 
-        direction: 'horizontal',
-        loop: true,
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-          },
+    slidesPerView: 5,
+    watchOverflow: true,
+    spaceBetween: 60,
+    centeredSlides: true,
+  });
+  $(function () {
+    $("#accordion").accordion(
+      {
+        collapsible: true,
+        active: false,
+        heightStyle: 'content',
+        animate: {
+          duration: 500
+        }
+      }
+    );
 
-          slidesPerView: 5,
-          watchOverflow: true,
-          spaceBetween: 95,
-          centeredSlides: true,
-    });
+  });
 
-    const accordionLinks = document.querySelectorAll('.accordion-item__link');
-    const accordionBtns = document.querySelectorAll('.accordion-item__btn');
-    for (let i = 0; i < accordionLinks.length; ++i) {
-      accordionLinks[i].addEventListener('click', function () {
-        accordionBtns[i].classList.toggle('cross');
-      })
-    }
+  const accordionLinks = document.querySelectorAll('.accordion-item__link');
+  const accordionBtns = document.querySelectorAll('.accordion-item__button');
+  for (let i = 0; i < accordionLinks.length; ++i) {
+    accordionLinks[i].addEventListener('click', function () {
+      for (let j = 0; j < i; ++j) {
+        accordionBtns[j].classList.remove('up');
+      }
+      for (let j = i+1; j < accordionLinks.length; ++j) {
+        accordionBtns[j].classList.remove('up');
+      }
+      accordionBtns[i].classList.toggle('up');
 
+    })
+  }
+
+  let gallerySwiper = new Swiper('.swiper-container', {
+    // slideClass: 'gallery-slide',
+    // wrapperClass: 'gallery-wrapper',
+    direction: 'horizontal',
+    loop: true,
+    navigation: {
+      nextEl: '.gallery-button-next',
+      prevEl: '.gallery-button-prev',
+    },
+    pagination: {
+      el: '.gallery-pagination',
+      type: 'fraction',
+    },
+
+    slidesPerView: 2.3,
+    watchOverflow: true,
+    spaceBetween: 30,
+    centeredSlides: true,
+  });
+
+  let modalForm = document.querySelector('.modal');
+  let modalBtn = document.querySelector('#banner-btn');
+  let modalX = document.querySelector('.modal-x');
+  modalBtn.addEventListener('click', function(){
+    modalForm.classList.add('modal--open');
+  })
+  modalX.addEventListener('click', function(){
+    modalForm.classList.remove('modal--open');
+  }
+  )
 
 })
